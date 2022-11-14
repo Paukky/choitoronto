623225b09c200f0e08ee6b3321e87bcf5d896cd2
Paul <paul.ly@live.ca>
2022-11-14 06:34:38 -0500

    Added background and changed colourSchemes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import * as React from "react"
-import {ChakraProvider,theme,} from "@chakra-ui/react"
+import {ChakraProvider,theme,Image} from "@chakra-ui/react"
 import { Navbar } from "./layout/Navbar"
 import { Landing } from "./components/Landing"
 import { Menu } from "./components/Menu"
+import Background from "./assets/background6.jpg"
 import {
   BrowserRouter as Router,
   Route,
@@ -11,8 +12,8 @@ import {
 
 export const App = () => (
 
-
   <ChakraProvider theme={theme}>
+    <Image src={Background} zIndex={-1} position={'absolute'} sx={{backgroundSize: 'cover'}} height={'120vh'} width={'100vw'}/>
     <Router>
       <Navbar/> 
       <Routes>
--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -21,29 +21,29 @@ export const Landing = () => {
 
     return (
         <Container maxWidth={{base:'container.sm', md:'container.lg'}} marginY={'1em'} centerContent >
-            <Text textAlign={'center'} fontSize={'4xl'} maxWidth={'35ch'} color={'red.600'}><b>We share our fond memories of the Vietnamese streets, in food language</b></Text>
+            <Text textAlign={'center'} fontSize={'4xl'} maxWidth={'35ch'}><b>We share our fond memories of the Vietnamese streets, in food language</b></Text>
             <Stack marginY={'1em'} direction={{base:'column', md:'row'}} spacing={'20px'}>
                 <Box display={'flex'} alignItems={'center'} flexDirection={{base:'column', md:'row'}}>
                     <Icon as={MdDeliveryDining} fontSize={'6xl'} marginRight={{ base: 'none',md:'0.5em'}}/>
-                    <Text maxWidth={'30ch'} textAlign={'center'} color={'red.600'}>Delivery on UberEats, SkipTheDish, DoorDash</Text>
+                    <Text maxWidth={'30ch'} textAlign={'center'} >Delivery on UberEats, SkipTheDish, DoorDash</Text>
                 </Box>
                 <Box display={'flex'} alignItems={'center'} justifyItems={'center'} flexDirection={{base:'column', md:'row'}}>
                     <Icon as={GiPartyPopper} fontSize={'6xl'} marginRight={{ base: 'none',md:'0.5em'}} />
-                    <Text maxWidth={'30ch'} color={'red.600'}>Party reservation available</Text>
+                    <Text maxWidth={'30ch'} >Party reservation available</Text>
                 </Box>
                 <Box display={'flex'} alignItems={'center'} flexDirection={{base:'column', md:'row'}}>
                     <Icon as={FaGlassCheers} fontSize={'6xl'} marginRight={{ base: 'none',md:'0.5em'}}/>
-                    <Text maxWidth={'30ch'} color={'red.600'}>Alcohol is available</Text>
+                    <Text maxWidth={'30ch'}>Alcohol is available</Text>
                 </Box>
             </Stack>   
             <Box display={'flex'} marginY={'1em'} flexDirection={{base:'column-reverse', md:'row'}}>
 
                 <Box flexDirection={'column'} flexWrap={'wrap'} marginX={'1em'}>
-                    <Text fontSize={'6xl'} textAlign={'center'} color={'red.600'}>Our Story</Text>
-                    <Text my={'0.5em'} textAlign={'center'} maxWidth={{base:'none',md:'35ch'}} color={'red.600'}>
+                    <Text fontSize={'6xl'} textAlign={'center'} >Our Story</Text>
+                    <Text my={'0.5em'} textAlign={'center'} maxWidth={{base:'none',md:'35ch'}}>
                         We’re a group of 4 lost individuals with different occupations. Still, when we came together, we realized we all have something in common, <i>our love for Vietnamese food,</i> especially <b>Vietnamese street food.</b>
                     </Text>
-                    <Text my={'0.5em'} textAlign={'center'} maxWidth={{base:'none',md:'35ch'}} color={'red.600'}>
+                    <Text my={'0.5em'} textAlign={'center'} maxWidth={{base:'none',md:'35ch'}}>
                         Over the years, Pho has always been everyone’s #1 choice when we speak of Vietnamese food. We’re not satisfied yet.<b> We want to introduce you to more specialties, cultural desserts, snacks and other appetizers that have been overlooked.</b>
                         
                     </Text>
